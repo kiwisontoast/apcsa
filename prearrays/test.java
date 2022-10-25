e52f011e29f9afb583b7238b3351ed65e623ee3e
kiwisontoast <devbshroff@gmail.com>
2022-10-25 10:03:14 -0500

    random testing labs for class practice

---
+++ prearrays/test.java
@@ -0,0 +1,21 @@
+
+	public class test
+	{
+		public static void main(String args [])
+		{
+		Shape circle = new Shape();
+		Shape rectangle = new Shape();
+		System.out.println(circle.getArea(2));
+		System.out.println(circle.getPerimeter(2));
+		System.out.println(rectangle.getArea(5.0));
+		System.out.println(rectangle.getPerimeter(5.0));
+		}
+	}
+	
+
+
+	
+
+
+
+	
