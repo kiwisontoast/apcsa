e784504dc682e81af71e4dcb5f87c63d0dd6e2e3
kiwisontoast <devbshroff@gmail.com>
2023-01-07 15:49:53 -0600

    triangle one lab finished

--- nested loops/TriangleOne.java
+++ nested loops/TriangleOne.java
@@ -1,13 +1,19 @@
 
 
-public class TriangleOne
+public final class TriangleOne
 {
    //this lab is setup with a single static method
    //there are no instance variables or additional methods / constructors
 
-	public static String go( String let, int size)
+	public  static String go( String let, int size)
 	{
 		String output="";
+		for(int i=0;i<=size;i++){
+			for(int j=0;j<i;j++){
+				System.out.print(let);
+			}
+			System.out.println();
+		}
 		return output;
 	}
 }
--- nested loops/TriangleOneRunner.java
+++ nested loops/TriangleOneRunner.java
@@ -1,25 +1,26 @@
-
-
-import static java.lang.System.*;
+import java.lang.System.*;
 import java.util.Scanner;
 
 public class TriangleOneRunner
 {
+
 	public static void main( String args[] )
    {
-   	Scanner keyboard = new Scanner(System.in);
-   	String choice="";
-		do{
-			out.print("Enter the size of the triangle : ");
-			int big = keyboard.nextInt();
-			out.print("Enter a letter : ");
-			String value = keyboard.next();
 
+   		Scanner keyboard = new Scanner(System.in);
+	   	String choice="";
+		do{
+			System.out.print("Enter the size of the triangle : ");
+			int big = keyboard.nextInt();
+			System.out.print("Enter a letter : ");
+			String value = keyboard.next();
+			
 				//static methods can be called using the class name
-			System.out.println( TriangleOne.go( value, big ) );
+			System.out.println(TriangleOne.go( value, big ) );
 
 			System.out.print("Do you want to enter more data? ");
 			choice=keyboard.next();
 		}while(choice.equals("Y")||choice.equals("y"));
 	}
 }
+
